53a9328f4d4a4c1c5c9de7c23ae95c8d6123f6ce
Gaspar Rey <info@gasparrey.com>
2015-03-14 18:48:03 +0000

    Split vim config in several files

--- link.sh
+++ link.sh
@@ -1,27 +1,30 @@
 #!/bin/bash
 
-# System configuration
+# OSX
 ln -f -s ~/Dropbox/dotfiles/.bash_profile ~/.bash_profile
 ln -f -s ~/Dropbox/dotfiles/config ~/.ssh/config
 
-# Git configuration
+# git
 ln -f -s ~/Dropbox/dotfiles/.gitconfig ~/.gitconfig
 ln -f -s ~/Dropbox/dotfiles/.gitignore_global ~/.gitignore_global
-
-# Git completion
 ln -f -s ~/Dropbox/dotfiles/.git-completion.bash ~/.git-completion.bash
 
-# Other configuration files
+# npm
 ln -f -s ~/Dropbox/dotfiles/.npmrc ~/.npmrc
+
+# Vim
 ln -f -s ~/Dropbox/dotfiles/.vimrc ~/.vimrc
+ln -f -s ~/Dropbox/dotfiles/.vimrc.vundle ~/.vimrc.vundle
+ln -f -s ~/Dropbox/dotfiles/.vimrc.basic ~/.vimrc.basic
+ln -f -s ~/Dropbox/dotfiles/.vimrc.nerdtree ~/.vimrc.nerdtree
+ln -f -s ~/Dropbox/dotfiles/.vimrc.ctrlp ~/.vimrc.ctrlp
+ln -f -s ~/Dropbox/dotfiles/.vimrc.bufexplorer ~/.vimrc.bufexplorer
+ln -f -s ~/Dropbox/dotfiles/.vimrc.nerdcommenter ~/.vimrc.nerdcommenter
+ln -f -s ~/Dropbox/dotfiles/.vimrc.neocomplcache ~/.vimrc.neocomplcache
 
 # Sublime Text
 ln -f -s ~/Dropbox/dotfiles/Default.sublime-theme ~/Library/Application\ Support/Sublime\ Text\ 3/Packages/User/Default.sublime-theme
 ln -f -s ~/Dropbox/dotfiles/Default\ \(OSX\).sublime-keymap ~/Library/Application\ Support/Sublime\ Text\ 3/Packages/User/Default\ \(OSX\).sublime-keymap
 ln -f -s ~/Dropbox/dotfiles/Package\ Control.sublime-settings ~/Library/Application\ Support/Sublime\ Text\ 3/Packages/User/Package\ Control.sublime-settings
 ln -f -s ~/Dropbox/dotfiles/Preferences.sublime-settings ~/Library/Application\ Support/Sublime\ Text\ 3/Packages/User/Preferences.sublime-settings
-
-# Make Sublime Text command line
-# .bash_profile
-# export PATH=/usr/local/bin:$PATH
 ln -f -s /Applications/Sublime\ Text.app/Contents/SharedSupport/bin/subl /usr/local/bin/subl
