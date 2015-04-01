592ebb18113850ffaa83c815251657cdb5b17b84
Gaspar Rey <info@gasparrey.com>
2015-04-01 19:48:59 +0100

    Create folders before making links

--- link.sh
+++ link.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 # OSX
+mkdir -p ~/.ssh
 ln -f -s ~/Dropbox/dotfiles/.bash_profile ~/.bash_profile
 ln -f -s ~/Dropbox/dotfiles/config ~/.ssh/config
 
@@ -24,10 +25,13 @@ ln -f -s ~/Dropbox/dotfiles/.vimrc.ultisnips ~/.vimrc.ultisnips
 ln -f -s ~/Dropbox/dotfiles/.vimrc.indentline ~/.vimrc.indentline
 
 # Sublime Text
+ln -f -s /Applications/Sublime\ Text.app/Contents/SharedSupport/bin/subl /usr/local/bin/subl
+mkdir -p ~/Library/Application\ Support/Sublime\ Text\ 3
+mkdir -p ~/Library/Application\ Support/Sublime\ Text\ 3/Packages
+mkdir -p ~/Library/Application\ Support/Sublime\ Text\ 3/Packages/User
+mkdir -p ~/Library/Application\ Support/Sublime\ Text\ 3/Packages/JavaScript
 ln -f -s ~/Dropbox/dotfiles/Default.sublime-theme ~/Library/Application\ Support/Sublime\ Text\ 3/Packages/User/Default.sublime-theme
 ln -f -s ~/Dropbox/dotfiles/Default\ \(OSX\).sublime-keymap ~/Library/Application\ Support/Sublime\ Text\ 3/Packages/User/Default\ \(OSX\).sublime-keymap
 ln -f -s ~/Dropbox/dotfiles/Package\ Control.sublime-settings ~/Library/Application\ Support/Sublime\ Text\ 3/Packages/User/Package\ Control.sublime-settings
 ln -f -s ~/Dropbox/dotfiles/Preferences.sublime-settings ~/Library/Application\ Support/Sublime\ Text\ 3/Packages/User/Preferences.sublime-settings
-ln -f -s /Applications/Sublime\ Text.app/Contents/SharedSupport/bin/subl /usr/local/bin/subl
-mkdir -p ~/Library/Application\ Support/Sublime\ Text\ 3/Packages/JavaScript
 ln -f -s ~/Dropbox/dotfiles/JSON.tmLanguage ~/Library/Application\ Support/Sublime\ Text\ 3/Packages/JavaScript/JSON.tmLanguage
