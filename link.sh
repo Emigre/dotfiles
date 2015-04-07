7c4b14eca7ec050778c8623fa61b73467a80b60e
Gaspar Rey <info@gasparrey.com>
2015-04-07 22:03:00 +0100

    Add missing tmLanguage files for Sublime

--- link.sh
+++ link.sh
@@ -29,9 +29,19 @@ ln -f -s /Applications/Sublime\ Text.app/Contents/SharedSupport/bin/subl /usr/lo
 mkdir -p ~/Library/Application\ Support/Sublime\ Text\ 3
 mkdir -p ~/Library/Application\ Support/Sublime\ Text\ 3/Packages
 mkdir -p ~/Library/Application\ Support/Sublime\ Text\ 3/Packages/User
+mkdir -p ~/Library/Application\ Support/Sublime\ Text\ 3/Packages/C++
+mkdir -p ~/Library/Application\ Support/Sublime\ Text\ 3/Packages/CSS
+mkdir -p ~/Library/Application\ Support/Sublime\ Text\ 3/Packages/Java
 mkdir -p ~/Library/Application\ Support/Sublime\ Text\ 3/Packages/JavaScript
+mkdir -p ~/Library/Application\ Support/Sublime\ Text\ 3/Packages/Python
+mkdir -p ~/Library/Application\ Support/Sublime\ Text\ 3/Packages/Ruby
 ln -f -s ~/Dropbox/dotfiles/Default.sublime-theme ~/Library/Application\ Support/Sublime\ Text\ 3/Packages/User/Default.sublime-theme
 ln -f -s ~/Dropbox/dotfiles/Default\ \(OSX\).sublime-keymap ~/Library/Application\ Support/Sublime\ Text\ 3/Packages/User/Default\ \(OSX\).sublime-keymap
 ln -f -s ~/Dropbox/dotfiles/Package\ Control.sublime-settings ~/Library/Application\ Support/Sublime\ Text\ 3/Packages/User/Package\ Control.sublime-settings
 ln -f -s ~/Dropbox/dotfiles/Preferences.sublime-settings ~/Library/Application\ Support/Sublime\ Text\ 3/Packages/User/Preferences.sublime-settings
-ln -f -s ~/Dropbox/dotfiles/JSON.tmLanguage ~/Library/Application\ Support/Sublime\ Text\ 3/Packages/JavaScript/JSON.tmLanguage
+ln -f -s ~/Dropbox/dotfiles/sublime/C++/* ~/Library/Application\ Support/Sublime\ Text\ 3/Packages/C++/
+ln -f -s ~/Dropbox/dotfiles/sublime/CSS/* ~/Library/Application\ Support/Sublime\ Text\ 3/Packages/CSS/
+ln -f -s ~/Dropbox/dotfiles/sublime/Java/* ~/Library/Application\ Support/Sublime\ Text\ 3/Packages/Java/
+ln -f -s ~/Dropbox/dotfiles/sublime/JavaScript/* ~/Library/Application\ Support/Sublime\ Text\ 3/Packages/JavaScript/
+ln -f -s ~/Dropbox/dotfiles/sublime/Python/* ~/Library/Application\ Support/Sublime\ Text\ 3/Packages/Python/
+ln -f -s ~/Dropbox/dotfiles/sublime/Ruby/* ~/Library/Application\ Support/Sublime\ Text\ 3/Packages/Ruby/
