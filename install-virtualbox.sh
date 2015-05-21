e2566270d6288e31bfd3ef7c28512fa6a775b928
Gaspar Rey <info@gasparrey.com>
2015-05-21 01:34:40 +0100

    Add installer for vagrant manager

--- install-virtualbox.sh
+++ install-virtualbox.sh
@@ -2,7 +2,7 @@
 
 brew cask install virtualbox
 brew cask install vagrant
+brew cask install vagrant-manager
 vagrant box add "Windows 7 with IE8 (32bit)" http://aka.ms/vagrant-win7-ie8
 vagrant box add "Windows 7 with IE9 (32bit)" http://aka.ms/vagrant-win7-ie9
 vagrant box add "Windows 7 with IE10 (32bit)" http://aka.ms/vagrant-win7-ie10
-vagrant box add "Windows 8.1 with IE11 (32bit)" http://aka.ms/vagrant-win81-ie11
