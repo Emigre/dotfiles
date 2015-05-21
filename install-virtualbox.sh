e16cf88e7e17626ff887fa23948ab10e7fc1220d
Gaspar Rey <info@gasparrey.com>
2015-05-21 21:15:26 +0200

    Add the hashicorp/precise32 box

--- install-virtualbox.sh
+++ install-virtualbox.sh
@@ -6,3 +6,4 @@ brew cask install vagrant-manager
 vagrant box add "Windows 7 with IE8 (32bit)" http://aka.ms/vagrant-win7-ie8
 vagrant box add "Windows 7 with IE9 (32bit)" http://aka.ms/vagrant-win7-ie9
 vagrant box add "Windows 7 with IE10 (32bit)" http://aka.ms/vagrant-win7-ie10
+vagrant box add hashicorp/precise32
