4560793ef14c3f2fd17586e0362d550909b761c7
Gaspar Rey <info@gasparrey.com>
2015-05-22 15:41:01 +0200

    Remove vagrant manager

--- install-virtualbox.sh
+++ install-virtualbox.sh
@@ -2,7 +2,6 @@
 
 brew cask install virtualbox
 brew cask install vagrant
-brew cask install vagrant-manager
 vagrant box add "Windows 7 with IE8 (32bit)" http://aka.ms/vagrant-win7-ie8
 vagrant box add "Windows 7 with IE9 (32bit)" http://aka.ms/vagrant-win7-ie9
 vagrant box add "Windows 7 with IE10 (32bit)" http://aka.ms/vagrant-win7-ie10
