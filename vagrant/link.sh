793041baecfcde6fc8dfd29685432c27bded9347
Gaspar Rey <info@gasparrey.com>
2015-05-22 16:07:32 +0200

    Add folder for vagrant files

---
+++ vagrant/link.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+mkdir -p ~/Vagrant
+mkdir -p ~/Vagrant/ie8
+mkdir -p ~/Vagrant/ie9
+mkdir -p ~/Vagrant/ie10
+ln -f -s ~/Dropbox/dotfiles/vagrant/ie8/Vagrantfile ~/Vagrant/ie8/Vagrantfile
+ln -f -s ~/Dropbox/dotfiles/vagrant/ie9/Vagrantfile ~/Vagrant/ie9/Vagrantfile
+ln -f -s ~/Dropbox/dotfiles/vagrant/ie10/Vagrantfile ~/Vagrant/ie10/Vagrantfile
