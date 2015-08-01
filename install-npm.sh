4dc9ee4860d6723ae94a5024bab298e049e42cd3
Gaspar Rey <info@gasparrey.com>
2015-08-01 18:36:33 +0100

    Install jspm

--- install-npm.sh
+++ install-npm.sh
@@ -9,34 +9,35 @@ sudo chown -R $USER ~/.npm
 sudo chown -R $USER ~/.node/lib/node_modules
 
 npm install bower -g
-npm install ember-cli -g
 npm install broccoli-cli -g
+npm install cordova -g
+npm install divshot-cli -g
+npm install ember-cli -g
 npm install generator-angular -g
-npm install generator-gulp-angular -g
 npm install generator-backbone -g
 npm install generator-backbone-mocha -g
 npm install generator-bootstrap -g
 npm install generator-extjs@ -g
+npm install generator-gulp-angular -g
 npm install generator-jekyllstarter -g
 npm install generator-karma -g
+npm install generator-magic -g
 npm install generator-mocha -g
 npm install generator-webapp -g
-npm install generator-magic -g
 npm install generator-wizardry -g
 npm install grunt-cli -g
 npm install gulp -g
+npm install http-server -g
+npm install ionic -g
+npm install jspm -g
 npm install karma-cli -g
+npm install less -g
 npm install mocha -g
+npm install n -g
 npm install npm-check-updates -g
 npm install phantomjs -g
 npm install webpack -g
 npm install yo -g
-npm install less -g
-npm install divshot-cli -g
-npm install http-server -g
-npm install cordova -g
-npm install ionic -g
-npm install n -g
 
 sudo n 0.10.40
 sudo n latest
