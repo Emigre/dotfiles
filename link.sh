fd8a7f10fcee877097df65dc652bec9fbf7cc421
Gaspar Rey <info@gasparrey.com>
2015-08-27 23:46:16 +0100

    Add .emacs file

--- link.sh
+++ link.sh
@@ -22,6 +22,9 @@ ln -f -s ~/Dropbox/dotfiles/.vimrc.ultisnips ~/.vimrc.ultisnips
 ln -f -s ~/Dropbox/dotfiles/.vimrc.indentline ~/.vimrc.indentline
 ln -f -s ~/Dropbox/dotfiles/.vimrc.buftabline ~/.vimrc.buftabline
 
+# Emacs
+ln -f -s ~/Dropbox/dotfiles/.emacs ~/.emacs
+
 # Sublime Text
 ln -f -s ~/Applications/Sublime\ Text.app/Contents/SharedSupport/bin/subl /usr/local/bin/subl
 mkdir -p ~/Library/Application\ Support/Sublime\ Text\ 3
