d7072de1223155dd1deb3e2aeaae31fce7177b00
Gaspar Rey <info@gasparrey.com>
2015-08-29 00:28:21 +0100

    Install tintin++ MUD client

--- install-brew.sh
+++ install-brew.sh
@@ -1,24 +1,25 @@
 #!/bin/bash
 
+brew install emacs --with-cocoa
 brew install git
 brew install httpie
-brew install mongodb
-brew install postgresql
-brew install sqlite
-brew install mysql
-brew install python3
-brew install vim
-brew install emacs --with-cocoa
-brew install pandoc
-brew install nginx
-brew install tree
 brew install mercurial
+brew install mongodb
+brew install mysql
+brew install nginx
+brew install pandoc
+brew install postgresql
+brew install python3
+brew install sqlite
+brew install tintin
 brew install tomcat
+brew install tree
+brew install vim
 
 brew tap homebrew/games
+brew install frobtads
 brew install frotz
 brew install glulxe
-brew install frobtads
 
 brew tap caskroom/cask
 brew install brew-cask
