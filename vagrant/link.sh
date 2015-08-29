436d351c5017b1cfd36155b1499ef81c60cf51f2
Gaspar Rey <info@gasparrey.com>
2015-08-29 01:52:13 +0100

    Add Ubuntu 14.04 'Trusty Tahr' virtual machine

--- vagrant/link.sh
+++ vagrant/link.sh
@@ -4,6 +4,8 @@ mkdir -p ~/Vagrant
 mkdir -p ~/Vagrant/ie8
 mkdir -p ~/Vagrant/ie9
 mkdir -p ~/Vagrant/ie10
+mkdir -p ~/Vagrant/ubuntu-1404-desktop
 ln -f -s ~/Dropbox/dotfiles/vagrant/ie8/Vagrantfile ~/Vagrant/ie8/Vagrantfile
 ln -f -s ~/Dropbox/dotfiles/vagrant/ie9/Vagrantfile ~/Vagrant/ie9/Vagrantfile
 ln -f -s ~/Dropbox/dotfiles/vagrant/ie10/Vagrantfile ~/Vagrant/ie10/Vagrantfile
+ln -f -s ~/Dropbox/dotfiles/vagrant/ubuntu-1404-desktop/Vagrantfile ~/Vagrant/ubuntu-1404-desktop/Vagrantfile
