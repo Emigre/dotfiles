c4c7d00a10a9bb886f00415adb94e667b67c0045
Gaspar Rey <info@gasparrey.com>
2015-08-29 15:41:23 +0100

    Add YACReader

--- install-cask.sh
+++ install-cask.sh
@@ -2,15 +2,17 @@
 
 brew tap caskroom/fonts
 brew tap caskroom/versions
-brew cask install sublime-text3
-brew cask install font-meslo-lg
-brew cask install evernote
-brew cask install dash
-brew cask install caffeine
-brew cask install vlc
-brew cask install the-unarchiver
-brew cask install java
-brew cask install iterm2
-brew cask install totalspaces
-brew cask install zoom
+
 brew cask install arduino
+brew cask install caffeine
+brew cask install dash
+brew cask install evernote
+brew cask install font-meslo-lg
+brew cask install iterm2
+brew cask install java
+brew cask install sublime-text3
+brew cask install the-unarchiver
+brew cask install totalspaces
+brew cask install vlc
+brew cask install yacreader
+brew cask install zoom
