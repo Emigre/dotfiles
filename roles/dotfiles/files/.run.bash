bedf9b8597c8959badb593d4e09d719cccb9791a
Gaspar Rey <info@gasparrey.com>
2018-02-10 19:54:38 +0000

    Search for a binary name in CMakeLists.txt in the run function

--- roles/dotfiles/files/.run.bash
+++ roles/dotfiles/files/.run.bash
@@ -1,6 +1,18 @@
 # makes, executes and watches sources from inside the build folder
 function run() {
+  local binary_name=""
+  local cmake_path="."
   if [ $# -eq 0 ]; then
+    if [[ `basename "$PWD"` == 'build' ]]; then
+      cmake_path='..'
+    fi
+    if [[ -f "$cmake_path/CMakeLists.txt" ]]; then
+      binary_name=`grep -o -m 1 "set(APP_NAME \(\w\+\))" $cmake_path/CMakeLists.txt | sed -E "s/.*\(APP_NAME ([^']+)\).*/\\1/"`
+    fi
+  else
+    binary_name=$@
+  fi
+  if [[ $binary_name == "" ]]; then
     echo "✗ Error: No binary name provided"
     return
   fi
@@ -12,6 +24,6 @@ function run() {
       return
     fi
   fi
-  ag -l . .. | entr -c -d -s "make && ./$@"
+  ag -l . .. | entr -c -d -s "make && ./$binary_name"
 }
 
