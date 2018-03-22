8314d9c675d965d4636093f8ac3500c7b954bfdb
Gaspar Rey <info@gasparrey.com>
2018-03-22 15:08:49 +0000

    Move my vagrant files and the scripts to separate repos

--- roles/scripts/files/cmake_build.bash
+++
@@ -1,9 +0,0 @@
-# Runs CMake in a build folder
-function cmake_build() {
-  if [[ ! `basename "$PWD"` == 'build' ]]; then
-    mkdir -p build
-    cd build
-  fi
-  cmake -D CMAKE_BUILD_TYPE=Debug ../
-}
-
