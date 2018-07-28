27156fcb2071875690ec91b3ee24e9fd7544568c
Gaspar Rey <info@gasparrey.com>
2018-07-28 22:43:05 +0100

    Do not autocomplete the h script after the first argument

--- roles/main/files/h_completion.bash
+++ roles/main/files/h_completion.bash
@@ -1,8 +1,18 @@
+# A .h_config file should be in the home folder with the different APIs listed in this format:
+# <name> <api_url> [--spec <swagger_url>]
+# Example:
+# github https://api.github.com --spec https://api.apis.guru/v2/specs/github.com/v3/swagger.json
+
 _http_prompt() {
   if [ -f $HOME/.h_config ]; then
-    cur_word=${COMP_WORDS[COMP_CWORD]}
-    comp_options=`cat $HOME/.h_config | awk '{ print $1 }' | tr '\n' ' '`
-    COMPREPLY=( $(compgen -W "${comp_options}" -- ${cur_word}) )
+    if [ $COMP_CWORD -eq 1 ]; then
+      cur_word=${COMP_WORDS[COMP_CWORD]}
+      comp_options=`cat $HOME/.h_config | awk '{ print $1 }' | tr '\n' ' '`
+      COMPREPLY=( $(compgen -W "${comp_options}" -- ${cur_word}) )
+    else
+      # do not autocomplete parameters other than the first one
+      COMPREPLY=()
+    fi
   fi
   return 0
 }
